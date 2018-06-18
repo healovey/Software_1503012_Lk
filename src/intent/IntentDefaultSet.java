4e7a125ce4f6f793f65a6a9ff81061794316bf6b
LCY <lichunyang_1@outlook.com>
2018-06-18 13:15:39 +0800

    add intent

---
+++ src/intent/IntentDefaultSet.java
@@ -0,0 +1,51 @@
+package intent;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+//import java.io.*;
+import java.util.HashMap;
+
+public class IntentDefaultSet {
+	
+	public HashMap<String, String> aliasname;
+	public String[] exenames;
+	public boolean useexename;
+	
+	public IntentDefaultSet() {
+		readAlias();
+		useexename = false;
+	}
+	
+	public IntentDefaultSet(String[] exenames) {
+		
+		this.exenames = exenames;
+		useexename = true;
+		readAlias();
+
+	}
+	
+	private void readAlias() {
+		aliasname = new HashMap<String, String>();
+		try {
+			File file = new File("data/aliasname");  
+			BufferedReader reader = new BufferedReader(new FileReader(file));  
+			String tempString = null;  
+			while ((tempString = reader.readLine()) != null) {  
+				 String[] t = tempString.split(":");
+				 if(t.length != 2) {
+					 System.out.println("error in aliasname: " + tempString);
+					 continue;
+				 }
+				 aliasname.put(t[0], t[1]);
+			}
+			reader.close();
+		}catch (IOException e) {
+			// TODO: handle exception
+		}finally {
+			
+		}
+	}
+	
+}
---
+++ src/intent/IntentGet.java
@@ -0,0 +1,79 @@
+package intent;
+
+
+public class IntentGet {
+	
+	public String origin;
+	public String name;
+	public String intent;
+	public float score;
+	
+	private IntentDefaultSet set;
+	
+	public IntentGet(IntentDefaultSet set) {
+		this.set = set;
+	}
+	public IntentGet() {
+		this.set = new IntentDefaultSet();
+	}
+	
+	public boolean ifname() {
+		if(name=="")
+			return false;
+		else
+			return true;
+	}
+	
+	public void out() {
+		System.out.println("strs: "+ origin);
+		System.out.println("\tintent: " + intent + "\tscore: " + score + "\tname: " + getSoftwareName(true));
+	}
+	
+	public String getSoftwareName() {
+		return getSoftwareName(false);
+	}
+	public String getSoftwareName(boolean doit) {
+		String name;
+		if(this.name == "") {
+			name = origin;
+			doit = true;
+		}else {
+			name = this.name;
+		}
+			
+		if(doit) {
+			String[] r = name.split("打开");
+			if (r.length != 1) {
+				name = r[r.length - 1];
+				r = name.split("好吗|。");
+				name = r[0];
+			}
+		}
+		name = set.aliasname.getOrDefault(name, name);
+		if(set.useexename) {
+			float[] nameper = new float[set.exenames.length];
+			for(int i = 0; i < set.exenames.length - 1; i++) {
+				if(set.exenames[i].matches(name)) {
+					nameper[i] = name.length() / set.exenames[i].length();
+				}else {
+					nameper[i] = 0;
+				}
+			}
+			int maxid = -1;
+			float maxnum = -1;
+			for(int i = 0; i < nameper.length - 1; i++) {
+				if(nameper[i] > maxnum) {
+					maxnum = nameper[i];
+					maxid = i;
+				}
+			}
+			if(maxnum != -1) {
+				name = set.exenames[maxid];
+			}else {
+				name = "";
+			}
+		}
+		
+		return name;
+	}
+}
